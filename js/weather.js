0816c5f9c93a0c74da43dc3883a174f531797c2f
kth2642 <kth2642@naver.com>
2021-11-10 02:25:29 +0900

    Geolocation
    Add Geolocation info

---
+++ js/weather.js
@@ -0,0 +1,19 @@
+const API_KEY = "f4da7b2b09e5c26186eefea9007d6ce4";
+
+function onGeoOk(position) {
+    const lat = position.coords.latitude;
+    const lon = position.coords.longitude;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+    fetch(url).then(response => response.json()).then(data => {
+        const weatherContainer = document.querySelector("#weather span:first-child");
+        const city = document.querySelector("#weather span:last-child");
+        city.innerText = data.name
+        weatherContainer.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+    });
+}
+
+function onGeoError() {
+    alert("Can't find you. No weather for you.");
+}
+
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
